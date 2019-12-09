bc8169b5da94fc1350a0c624c628995a9833d260
柳江浩 <39641581+LJHAAAAA@users.noreply.github.com>
2019-12-09 21:43:02 +0800

    Add files via upload

--- houtai/src/App.js
+++ houtai/src/App.js
@@ -3,13 +3,14 @@ import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Login from './container/Login';
 import Home from './container/Home';
 
+
 export default class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
                     <Route path="/" exact component={Login} />
-                    <Route path="/home/admin" component={Home} />
+                    <Route path="/home" component={Home} />
                 </Switch>
             </Router>
         )
