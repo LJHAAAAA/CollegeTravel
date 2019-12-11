51791683f0a857475d49d52267858186d21b4024
秦晓旭 <33643037+xiaoxuqin@users.noreply.github.com>
2019-12-11 21:06:32 +0800

    Update RaiderDetail.js

--- src/container/radiers/RaiderDetail.js
+++ src/container/radiers/RaiderDetail.js
@@ -1,67 +1,69 @@
 import React, { Component } from 'react'
 import { NavBar, Icon } from 'antd-mobile';
 import AppRaiders from './AppRaiders'
-import {Link} from 'react-router-dom'
-import {BrowserRouter as Router,Route} from 'react-router-dom'
-import {createBrowserHistory} from 'history';
+import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
 import Todolist from './Todolist';
 const his = createBrowserHistory();
 
 export default class RaiderDetail extends Component {
-    showDetail=()=>{
+    constructor(props) {
+        super(props);
+        this.state = {
+            details: decodeURIComponent(this.props.location.search),//执行解码，因为中文字符串往往在传递时被编码过了
+            raidersDetails: {}
+        }
+    }
+    componentDidMount = () => {
+        fetch('http://localhost:8080/getRaidersDetails' + this.state.details)
+            .then(res => res.json())
+            .then(
+                data => {
+                    this.setState({
+                        raidersDetails: data
+                    })
+                    console.log(data)
+                }
+            )
+    }
+    raidersDetailsFanhui = () => {
         his.push('/home');
         window.location.reload();
     }
     render() {
+        let details = this.state.raidersDetails;
+        var date = new Date().toLocaleDateString();
         return (
             <Router>
-                <div style={{width:'100vw',position:'fixed',top:'0'}}>
+                <div style={{ width: '100vw', position: 'fixed', top: '0' }}>
                     <Link to='/home'>
-                    <NavBar
-                    mode="dark"
-                    leftContent="<"
-                    onClick={this.showDetail}
-                    className='feng_nav'
-                    rightContent={[
-                        // <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
-                        <Icon key="1" type="ellipsis" />,
-                    ]}
-                    >师大地标，洗月…</NavBar>
+                        <NavBar
+                            mode="dark"
+                            leftContent="<"
+                            onClick={this.raidersDetailsFanhui}
+                            className='feng_nav'
+                        >{details.rTitleP+'...'}</NavBar>
                     </Link>
-                    
                 </div>
-                <div style={{width:'100vw',height:'10vh'}}>
-
+                <div style={{ width: '100vw', height: '10vh' }}></div>
+                <div>
+                    <p className='feng_detail_title'>{details.rTitleP + details.rTitleSpan}</p>
+                    <br/>
+                    <div className='feng_detail_writter'>
+                        <img src={'https://s2.ax1x.com/2019/12/04/QQhEVS.jpg'} className='feng_head' />
+                        <p>校校官方</p>
+                        <span>{date}</span>
+                    </div>
+                    <br/>
+                    <div className='feng_detail_content'>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;{details.rDetai1}</p>
+                        <img src={details.rImg} className='feng_chatu' />
+                        <p style={{marginTop:'3vw'}}>&nbsp;&nbsp;&nbsp;&nbsp;{details.rDetai2}</p>
+                    </div>
+                    <Todolist />
                 </div>
-            <div>
-                
-                <p className='feng_detail_title'>师大地标，洗月湖</p>
-                <div className='feng_detail_writter'>
-                    <img  src={'https://s2.ax1x.com/2019/12/04/QQhEVS.jpg'} className='feng_head'/>
-                    <p>校校官方</p>
-                    <span>2019-09-09</span>
-                </div>
-                <div className='feng_detail_content'>
-                    <p>
-                        师大地标师大地标师大,地标师大地标师,大地标师大,地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大
-                        地标师大地标师大地标师大地标师大地标,师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标
-                        师大地标师大地标师大地标师大.地标
-                    
-                    </p>
-                    <img  src={'https://s2.ax1x.com/2019/12/04/QQhEVS.jpg'} className='feng_chatu'/>
-                    <p>
-                    师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大
-                        地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标
-                        师大地标师大地标师大地标师大地标
-                    师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标
-                    师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标
-                    师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标师大地标
-                    </p>
-                </div>
-                <Todolist/>
-            </div>
-            <Route path='/AppRaiders' component={AppRaiders}/>
-            
+                <Route path='/AppRaiders' component={AppRaiders} />
             </Router>
         )
     }
