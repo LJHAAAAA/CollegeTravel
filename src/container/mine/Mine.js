c98c0b55d3bd93e256551e95c09bffb09134e4e8
秦晓旭 <33643037+xiaoxuqin@users.noreply.github.com>
2019-12-18 19:36:18 +0800

    new

--- src/container/mine/Mine.js
+++ src/container/mine/Mine.js
@@ -1,66 +1,61 @@
 import React, { Component, Fragment } from 'react'
 import { NavBar, Icon } from 'antd-mobile';
 import { createBrowserHistory } from 'history';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const his = createBrowserHistory();
 
 export default class Mine extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            data:{
-                username:'未登录',
-                Signature:'暂未编辑'
+        this.state = {
+            data: {
+                username: '未登录',
+                Signature: '暂未编辑'
             }
-        } 
-    }   
-
-
-    componentDidMount=()=>{
+        }
+    }
+    componentDidMount = () => {
         console.log(1)
         fetch("http://localhost:8080/getusername")
-        .then(res=>res.json())
-        .then(
-            (res)=>{
-                this.setState({
-                    data:res
-                })
-                console.log('res')
-                console.log(res)
-            }
-        )
-
-        fetch('http://localhost:8080/getEditor')
-        .then(res=>res.json())
-        .then(
-            (res)=>{
-                if(res){ 
+            .then(res => res.json())
+            .then(
+                (res) => {
                     this.setState({
-                        data:res
+                        data: res
                     })
-                    console.log('res');
-                    console.log(this.state.data)
-                }                
-            }
-        )
+                    console.log('res')
+                    console.log(res)
+                }
+            )
+        fetch('http://localhost:8080/getEditor')
+            .then(res => res.json())
+            .then(
+                (res) => {
+                    if (res) {
+                        this.setState({
+                            data: res
+                        })
+                        console.log('res');
+                        console.log(this.state.data)
+                    }
+                }
+            )
     }
     editor = () => {
         console.log(1)
         his.push('/MineTab/editor');
         window.location.reload();
     }
-
-    about = ()=>{
+    about = () => {
         his.push('/MineTab/about');
         window.location.reload();
     }
-    Exit = ()=>{
+    Exit = () => {
         var confirm = window.confirm("确认退出？");
-        if(confirm){
+        if (confirm) {
             his.push('/');
             window.location.reload();
-        }else{
-            
+        } else {
         }
     }
 
@@ -70,18 +65,17 @@ export default class Mine extends Component {
         return (
             <Fragment>
                 <NavBar
-                        style={{ zIndex: '999', backgroundColor: 'rgb(77,199,249)', position: 'fixed', width: '100vw' }}
-                        mode="light"
-                    ><span style={{color:'white',fontSize:'6.5vw'}}>我&nbsp;&nbsp;&nbsp;&nbsp;的</span></NavBar>
+                    style={{ zIndex: '999', backgroundColor: 'rgb(77,199,249)', position: 'fixed', width: '100vw' }}
+                    mode="light"
+                ><span style={{ color: 'white', fontSize: '6.5vw' }}>我&nbsp;&nbsp;&nbsp;&nbsp;的</span></NavBar>
                 <div><NavBar></NavBar></div>
                 <div className='lv_headImg'>
                     <img style={{ position: 'relative', width: '100vw' }} src='https://s2.ax1x.com/2019/12/17/QoBhee.jpg' />
                 </div>
                 <div style={{ backgroundColor: 'white' }}>
                     <div className="lv_touxiang">
-                        <img src={src}/>
+                        <img src={src} />
                     </div>
-
                     <p style={{ position: 'relative', top: '-6vh', textAlign: 'center' }}>{this.state.data.username}</p>
                     <p style={{ position: 'relative', top: '-5vh', textAlign: 'center' }}>{sig}</p>
                 </div>
@@ -92,7 +86,7 @@ export default class Mine extends Component {
                     </div>
                     <div className='qinMineEvery'>
                         <img src='https://s2.ax1x.com/2019/12/17/QoqulV.png' />
-                        <Link to='/tiezi/shoucang'><p>帖子收藏</p></Link> 
+                        <Link to='/tiezi/shoucang'><p>帖子收藏</p></Link>
                     </div>
                     <div className='qinMineEvery' onClick={this.editor}>
                         <img src='https://s2.ax1x.com/2019/12/17/QoqKyT.png' />
@@ -104,9 +98,6 @@ export default class Mine extends Component {
                     </div>
                     <button className='qinMineExit' onClick={this.Exit}>退出登录</button>
                 </div>
-
-
-
             </Fragment>
         )
     }
